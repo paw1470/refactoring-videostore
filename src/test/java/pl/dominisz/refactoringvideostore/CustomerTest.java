71745b9f32a7e86a9cb031ee69b4a2e65274c296
dominisz <dominisz@gmail.com>
2019-05-13 19:59:20 +0200

    More unit tests

--- src/test/java/pl/dominisz/refactoringvideostore/CustomerTest.java
+++ src/test/java/pl/dominisz/refactoringvideostore/CustomerTest.java
@@ -18,6 +18,53 @@ public class CustomerTest {
         assertEquals(expectedStatement, actualStatement);
     }
 
+    //TODO create parametrized tests instead of three following tests:
+    //TODO shouldCreateStatementWithRegularMovie
+    //TODO shouldCreateStatementWithNewReleaseMovie
+    //TODO shouldCreateStatementWithChildrensMovie
+
+    @Test
+    public void shouldCreateStatementWithRegularMovie() {
+        Customer customer = new Customer("customer");
+        customer.addRental(new Rental(new Movie("movie1", Movie.REGULAR), 10));
+
+        String expectedStatement = "Rental Record for customer\n"
+                                   + "\tmovie1\t14.0\n"
+                                   + "You owed 14.0\n"
+                                   + "You earned 1 frequent renter points\n";
+        String actualStatement = StatementCreator.create(customer);
+
+        assertEquals(expectedStatement, actualStatement);
+    }
+
+    @Test
+    public void shouldCreateStatementWithNewReleaseMovie() {
+        Customer customer = new Customer("customer");
+        customer.addRental(new Rental(new Movie("movie2", Movie.NEW_RELEASE), 20));
+
+        String expectedStatement = "Rental Record for customer\n"
+                                   + "\tmovie2\t60.0\n"
+                                   + "You owed 60.0\n"
+                                   + "You earned 2 frequent renter points\n";
+        String actualStatement = StatementCreator.create(customer);
+
+        assertEquals(expectedStatement, actualStatement);
+    }
+
+    @Test
+    public void shouldCreateStatementWithChildrensMovie() {
+        Customer customer = new Customer("customer");
+        customer.addRental(new Rental(new Movie("movie3", Movie.CHILDRENS), 30));
+
+        String expectedStatement = "Rental Record for customer\n"
+                                   + "\tmovie3\t42.0\n"
+                                   + "You owed 42.0\n"
+                                   + "You earned 1 frequent renter points\n";
+        String actualStatement = StatementCreator.create(customer);
+
+        assertEquals(expectedStatement, actualStatement);
+    }
+
     @Test
     public void shouldCreateStatementWithThreeMovies() {
         Customer customer = new Customer("customer");
